3be0257ae2713871a6d5bdc0eb4700592d7e8bd6
TOGoS <togos00@gmail.com>
2026-03-24 13:04:52 -0500

    import-from-url.nix

---
+++ import-from-url.nix
@@ -0,0 +1,3 @@
+import (builtins.fetchurl "http://togos-fbs.nuke24.net/uri-res/raw/urn:bitprint:KUZO4EPEGLHLKN3IYFQNII62JACSPKF5.KTWNS3ASWPRKPA5
+BWBOMXL4SFG4GBHNHJV63SQA/fourfivesix.nix")
+# Should result in [ 4 5 6 ]
