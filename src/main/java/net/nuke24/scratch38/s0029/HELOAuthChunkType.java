10749f028ce8c26495622fc289ce33836805598c
TOGoS <togos00@gmail.com>
2026-09-01 17:40:52 -0500

    More auth chunk types

--- src/main/java/net/nuke24/scratch38/s0029/HELOAuthChunkType.java
+++ src/main/java/net/nuke24/scratch38/s0029/HELOAuthChunkType.java
@@ -2,8 +2,16 @@ package net.nuke24.scratch38.s0029;
 
 enum HELOAuthChunkType {
 	NORMAL,
+	/** Just the "auth" at the beginning of a header line */
+	AUTH_HEADER,
+	AUTH_SCHEME,
+	/** Indicates the value in the auth header following a unrecognized auth scheme */
+	AUTH_UNKNOWN_DATA,
+	/** Minimum / maxumum nonce values, sent from server */
 	NONCE_MIN,
 	NONCE_MAX,
+	/** ACtual nonce value, sent to server */
 	NONCE,
+	/** Hash or placeholder of hash */
 	HASH
 }
